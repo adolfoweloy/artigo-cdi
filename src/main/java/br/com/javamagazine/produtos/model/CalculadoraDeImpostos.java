c3b07b7f9b1129689b888c045f48fbe66a24c32b
aeloy <adolfo.eloy@gmail.com>
2015-03-22 14:30:58 -0300

    Versao simplificada utilizando apenas if para controle de regiao

--- src/main/java/br/com/javamagazine/produtos/model/CalculadoraDeImpostos.java
+++ src/main/java/br/com/javamagazine/produtos/model/CalculadoraDeImpostos.java
@@ -2,8 +2,28 @@ package br.com.javamagazine.produtos.model;
 
 import java.math.BigDecimal;
 
-public interface CalculadoraDeImpostos {
+import br.com.javamagazine.region.Region;
+import br.com.javamagazine.region.ServerWebServiceClient;
 
-  public abstract BigDecimal calcular(BigDecimal valorTotal);
+public class CalculadoraDeImpostos {
+
+  public BigDecimal calcular(BigDecimal valorTotal) {
+      
+      // guarda o valor do percentual
+      BigDecimal percentual = BigDecimal.ZERO;
+      
+      // recupera a regiao através da api fornecida pelo servidor na nuvem.
+      ServerWebServiceClient swsClient = new ServerWebServiceClient();
+      Region bucketLocation = swsClient.getBucketLocation();
+      
+      // imposto de 15% para a região de sao paulo e 4% para US_West
+      if (bucketLocation.equals(Region.SA_SaoPaulo)) {
+          percentual = new BigDecimal("0.15");
+      } else if(bucketLocation.equals(Region.US_West)) {
+          percentual = new BigDecimal("0.04");
+      }
+      
+      return valorTotal.add(valorTotal.multiply(percentual));
+  }
 
 }
